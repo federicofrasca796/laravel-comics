2bfd8d35a831f3c0310da74bd76daa204ed1b859
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-12-17 15:22:12 +0100

    import imgs | move html in app.blade

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/single_comic', function () {
+    return view('single_comic');
+});
