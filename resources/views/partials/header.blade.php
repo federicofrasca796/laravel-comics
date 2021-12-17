929455f6c2d9363ffa827e825c6eee32c7088bc3
Federico Frascà <70528215+federicofrasca796@users.noreply.github.com>
2021-12-17 17:43:25 +0100

    add jumbo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,40 +1,45 @@
 <header id="site_header">
-    <div id="top_banner">
+    <div id="top_header">
+        <!-- Top banner -->
+        <div id="top_banner">
+            <div class="container">
+                <ul class="justify-content-end">
+                    <li>DC POWER&trade; VISAS</li>
+                    <li>ADDITIONAL DC SITES</li>
+                </ul>
+            </div>
+        </div>
+        <!-- Navbar -->
         <div class="container">
-            <ul class="justify-content-end">
-                <li>DC POWER&trade; VISAS</li>
-                <li>ADDITIONAL DC SITES</li>
-            </ul>
+            <nav class="row align-items-center" id="header_nav">
+                <div class="col">
+                    <img src="{{asset('img/dc-logo.png')}}" alt="DC logo" width="70px">
+                </div>
+                <div class="col h-100">
+
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li class="active"><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Collectibles</a></li>
+                        <li><a href="">Videos</a></li>
+                        <li><a href="">Fans</a></li>
+                        <li><a href="">News</a></li>
+                        <li><a href="">Shop</a></li>
+                    </ul>
+
+                </div>
+                <div class="col searchbar d-flex">
+                    <input type="search" name="header_search" id="header_search" placeholder="Search">
+                    <span><i class="fas fa-search"></i></span>
+                </div>
+            </nav>
         </div>
     </div>
-    <div class="container">
-        <!-- Top banner -->
 
-        <!-- Navbar -->
-        <nav class="row align-items-center" id="header_nav">
-            <div class="col">
-                <img src="{{asset('img/dc-logo.png')}}" alt="DC logo" width="70px">
-            </div>
-            <div class="col h-100">
+    <div class="jumbo">
 
-                <ul>
-                    <li><a href="">Characters</a></li>
-                    <li class="active"><a href="">Comics</a></li>
-                    <li><a href="">Movies</a></li>
-                    <li><a href="">TV</a></li>
-                    <li><a href="">Games</a></li>
-                    <li><a href="">Collectibles</a></li>
-                    <li><a href="">Videos</a></li>
-                    <li><a href="">Fans</a></li>
-                    <li><a href="">News</a></li>
-                    <li><a href="">Shop</a></li>
-                </ul>
-
-            </div>
-            <div class="col searchbar d-flex">
-                <input type="search" name="header_search" id="header_search" placeholder="Search">
-                <span><i class="fas fa-search"></i></span>
-            </div>
-        </nav>
     </div>
 </header>
